eb3bd0c60c9af191d4d6611055c5010272a21a15
Elvhors77 <140495287+Elvhors77@users.noreply.github.com>
2024-02-10 23:04:04 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,11 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xfac759cefe113e510d495e8ebefa1fa7b1c7b338';
+export const CONTRACT_ADDRESS = '0x1f2906a6fb92fd8622ea600a388e3510f585214d';
 export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
-  buttons: ['Free mint'],
+  buttons: ['GeekHouse mint'],
   image: `${SITE_URL}/opengraph-image.png`,
   post_url: `${SITE_URL}/api/frame`,
 });
